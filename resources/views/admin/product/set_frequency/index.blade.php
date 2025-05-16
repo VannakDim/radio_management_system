3a7cc2a9490e0a895464bfad6c5a1118585c07b8
Vannak Dim <vannakhannah@gmail.com>
2025-05-16 17:10:17 +0700

    fix paginate

--- resources/views/admin/product/set_frequency/index.blade.php
+++ resources/views/admin/product/set_frequency/index.blade.php
@@ -78,7 +78,7 @@
                             </x-slot>
                         </x-card-table>
                         <div class="mt-4 text-center mb-4">
-                            {{ $set_frequency->links('vendor.pagination.custom') }}
+                            {{ $set_frequency->links() }}
                         </div>
 
                         {{-- <p>{{ $radio }}</p> --}}
